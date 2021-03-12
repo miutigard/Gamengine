5359a1cce16ebbc4df5b49f485cdf79f92b1252d
Sebastra99 <seromam@gmail.com>
2021-03-12 14:13:03 +0100

    Dette er en test av commit

--- src/Gamengine/Main.java
+++ src/Gamengine/Main.java
@@ -3,6 +3,6 @@ package Gamengine;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test.");
+        System.out.println("Hei du er en snill person.");
     }
 }
