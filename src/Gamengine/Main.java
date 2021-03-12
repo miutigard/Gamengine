42f1b1ac1ba745f3010cbcc91b4fbc3ff4c78a03
Markus <markusinge1@gmail.com>
2021-03-12 14:16:32 +0100

    Final test of the commit and pushes.

--- src/Gamengine/Main.java
+++ src/Gamengine/Main.java
@@ -3,6 +3,6 @@ package Gamengine;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hei du er en snill person.");
+        System.out.println("Final test.");
     }
 }
