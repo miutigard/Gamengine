fe189fdee72b7d31e7bdbc54aba06095ea8d19a9
Markus <markusinge1@gmail.com>
2021-03-12 15:10:38 +0100

    Updated PlayerCharacter.

--- src/Gamengine/Character/PlayerCharacter.java
+++ src/Gamengine/Character/PlayerCharacter.java
@@ -2,7 +2,7 @@ package Gamengine.Character;
 
 public class PlayerCharacter {
 
-    String name;
+    String name, graphics, moveUpKey, moveDownKey, moveRightKey, moveLeftKey, attackKey;
     int hitPoints, attack, defence;
 
     public PlayerCharacter() { }
@@ -44,6 +44,80 @@ public class PlayerCharacter {
 
     public void setDefence(int defence) {
         this.defence = defence;
+    }
+
+    //Use filelocation of picture file to set as a string to get the graphic for the player character.
+    public String getGraphics() {
+        return graphics;
+    }
+
+    public void setGraphics(String graphics) {
+        this.graphics = graphics;
+    }
+
+    public String getMoveUpKey() {
+        return moveUpKey;
+    }
+
+    public void setMoveUpKey(String moveUpKey) {
+        this.moveUpKey = moveUpKey;
+    }
+
+    public String getMoveDownKey() {
+        return moveDownKey;
+    }
+
+    public void setMoveDownKey(String moveDownKey) {
+        this.moveDownKey = moveDownKey;
+    }
+
+    public String getMoveRightKey() {
+        return moveRightKey;
+    }
+
+    public void setMoveRightKey(String moveRightKey) {
+        this.moveRightKey = moveRightKey;
+    }
+
+    public String getMoveLeftKey() {
+        return moveLeftKey;
+    }
+
+    public void setMoveLeftKey(String moveLeftKey) {
+        this.moveLeftKey = moveLeftKey;
+    }
+
+    public String getAttackKey() {
+        return attackKey;
+    }
+
+    public void setAttackKey(String attackKey) {
+        this.attackKey = attackKey;
+    }
+
+    //Methods
+
+    //Move player character methods
+    public void moveUp(String moveUpKey) {
+
+    }
+
+    public void moveDown(String moveDownKey) {
+
+    }
+
+    public void moveRight(String moveRightKey) {
+
+    }
+
+    public void moveLeft(String moveLeftKey) {
+
+    }
+
+    //Attack with player character
+
+    public void attack(String attackKey) {
+
     }
 
     @Override
