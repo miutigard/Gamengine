ec808b2a572b97b16e9745bdcf4723e5435a33c7
Markus <markusinge1@gmail.com>
2021-03-12 15:36:52 +0100

    Updated level.

--- src/Gamengine/LevelDesign/Level.java
+++ src/Gamengine/LevelDesign/Level.java
@@ -3,5 +3,29 @@ package Gamengine.LevelDesign;
 public class Level {
 
     int levelX, levelY;
-    
+
+
+    //Methods
+
+    //Where playercharacter spawns on the level.
+
+    public void spawnPoint() {
+
+    }
+
+    //Where the level ends when the playercharacter walks on the grid.
+    public void endPoint() {
+
+    }
+
+    //Where an eventual NPC will be placed on a level.
+    public void placeNPC() {
+
+    }
+
+    //Creates the grid-based level with a size of levelX * levelY
+    public void createLevel() {
+
+    }
+
 }
